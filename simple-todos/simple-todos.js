c8e4ea0786f1e16841d87301aa782e6eac8d2345
joanne <joannexo@users.noreply.github.com>
2015-08-16 14:14:15 -0700

    checklist and delete tasks

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -25,6 +25,17 @@ if (Meteor.isClient) {
     }
   }); 
 
+  Template.task.events({
+    "click .toggle-checked": function() {
+      Tasks.update(this._id, {
+        $set: {checked: ! this.checked}
+      }); 
+    }, 
+    "click .delete": function () {
+      Tasks.remove(this._id); 
+    }
+  }); 
+
 
 }
 
