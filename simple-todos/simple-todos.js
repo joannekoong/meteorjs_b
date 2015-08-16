04bcfc2b6816d26a557c20b1d9f8079fe46d6b47
joanne <joannexo@users.noreply.github.com>
2015-08-16 14:39:05 -0700

    hide incomplete

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -4,7 +4,19 @@ if (Meteor.isClient) {
 
   Template.body.helpers({
       tasks: function() {
-        return Tasks.find({}, {sort: {createdAt: -1}}); 
+        if (Session.get("hideCompleted")) {
+          return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});  
+                  
+        }
+        else {
+          return Tasks.find({}, {sort: {createdAt: -1}}); 
+        }
+      }, 
+      hideCompleted: function() {
+        return Session.get("hideCompleted"); 
+      }, 
+      incompleteCount: function() {
+        return Tasks.find({checked: {$ne: true}}).count();
       }
   });
 
@@ -22,7 +34,11 @@ if (Meteor.isClient) {
 
       event.target.text.value = ""; 
 
+    }, 
+    "change .hide-completed input": function(event) {
+        Session.set("hideCompleted", event.target.checked); 
     }
+
   }); 
 
   Template.task.events({
