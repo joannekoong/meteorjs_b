5830644d0b8fc82caff67afaed7255c15e25528e
joanne <joannexo@users.noreply.github.com>
2015-08-17 16:34:08 -0700

    add increment functionality

--- leaderboard/leaderboard.js
+++ leaderboard/leaderboard.js
@@ -20,7 +20,10 @@ if (Meteor.isClient) {
     'click .player': function() {
       var playerId = this._id; 
       Session.set('selectedPlayer', playerId);
-
+    }, 
+    'click .increment': function() {
+      var selectedPlayer = Session.get('selectedPlayer'); 
+      PlayersList.update(selectedPlayer, {$inc: {score: 5} }); 
     }
   });  
 
