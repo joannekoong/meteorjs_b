67dbdc856032fd8900f706c6e446caa605abd58a
joanne <joannexo@users.noreply.github.com>
2015-08-17 17:49:04 -0700

    implement security

--- leaderboard/leaderboard.js
+++ leaderboard/leaderboard.js
@@ -20,6 +20,11 @@ if (Meteor.isServer) {
     'removePlayerData': function(selectedPlayer) {
       var currentUserId = Meteor.userId(); 
       PlayersList.remove({_id: selectedPlayer, createdBy: currentUserId}); 
+    }, 
+    'modifyPlayerScore': function(selectedPlayer, scoreValue) {
+      var currentUserId = Meteor.userId(); 
+      PlayersList.update({_id: selectedPlayer, createdBy: currentUserId}, {$inc: {score: scoreValue}}); 
+
     }
 
 
@@ -53,15 +58,15 @@ if (Meteor.isClient) {
     }, 
     'click .increment': function() {
       var selectedPlayer = Session.get('selectedPlayer'); 
-      PlayersList.update(selectedPlayer, {$inc: {score: 5} }); 
+      Meteor.call('modifyPlayerScore', selectedPlayer, 5); 
     }, 
     'click .decrement': function() {
       var selectedPlayer = Session.get('selectedPlayer'); 
-      PlayersList.update(selectedPlayer, {$inc: {score: -5} }); 
+      Meteor.call('modifyPlayerScore', selectedPlayer, -5); 
     }, 
     'click .remove': function() {
       var selectedPlayer = Session.get('selectedPlayer'); 
-      Meteor.call('removePlayerData', selectedPlayer)
+      Meteor.call('removePlayerData', selectedPlayer); 
     }
   });  
 
